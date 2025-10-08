c658784c28e0605d33437395a3a72ee258a3522c
Sean McClure <sean.mcclure17@gmail.com>
2025-10-08 18:32:53 -0400

    Remove devtools lol

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,6 @@ import {
   Scripts,
   createRootRouteWithContext,
 } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
 
@@ -58,20 +54,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         />
       </head>
       <body>
-          {children}
-          <TanStackDevtools
-            config={{
-              position: 'bottom-left',
-            }}
-            plugins={[
-              {
-                name: 'Tanstack Router',
-                render: <TanStackRouterDevtoolsPanel />,
-              },
-              TanStackQueryDevtools,
-            ]}
-          />
-          <Scripts />
+        {children}
+        <Scripts />
       </body>
     </html>
   )
